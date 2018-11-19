91511a43bcc4e5aae9abd634ea0e1ccb39488e94
Nathan <nathan.moehring@gmail.com>
2018-11-19 10:49:07 -0600

    Added some comments.

--- custom_class.cpp
+++ custom_class.cpp
@@ -19,17 +19,18 @@ private:
 
 class Farm {
 public:
-	Farm(int);
+	Farm(int numCreatures = 0); //Farm empty by default
 	~Farm();
 	void rollCall();
 	void populate(int);
 private:
-	vector<Creature*> m_population;
+	vector<Creature*> m_population; //unnecessary but shows what I learned from the chapter
 };
 
 int main() {
 	srand(time(0));
 
+	//Create 5 randomly named creatures in the farm
 	Farm creatureFarm(5);
 	creatureFarm.rollCall();
 
@@ -50,10 +51,14 @@ string Creature::getName() {
 
 void Farm::populate(int numCreatures) {
 	for (int x = 0; x < numCreatures; x++) {
+		//push a pointer of a new creature on the heap and pass a randomly
+		//selected name to the constructor
 		m_population.push_back(new Creature(names[rand() % names.size()]));
 	}
 }
 
+//iterate through all creatures in the farm, have them give their name and
+// address on the heap
 void Farm::rollCall() {
 	for (int x = 0; x < m_population.size(); x++) {
 		cout << m_population[x]->getName() << " at pen " << m_population[x];
@@ -66,6 +71,8 @@ Farm::Farm(int numCreatures) {
 }
 
 Farm::~Farm() {
+	//iterate through creatures on farm and have them say bye before freeing
+	// up their heap memory
 	for (int x = 0; x < m_population.size(); x++) {
 		cout << m_population[x]->getName() << ": 'Bye!'" << endl;
 		delete m_population[x];
